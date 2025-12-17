682a390dfd7dbc3759500e3b4eb58a939dacb048
Vladimir <vlap-d@yandex.ru>
2025-12-17 16:58:07 +0500

    Executable cust_2.sh

---
+++ cust_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+systemctl stop apache2; systemctl start nginx; 
+\cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled;
+\cp index.html /var/www/html; systemctl start apache2; 
+\cp filebeat.yml /etc/filebeat; systemctl restart filebeat; 
+\cp mysqld.cnf /etc/mysql/mysql.conf.d; service mysql restart; 
+\cp prometheus.yml /etc/prometheus; systemctl restart prometheus; 
+\systemctl restart prometheus-node-exporter; 
+\systemctl restart grafana-server; ./source.sh
